81a8999136336a3b6742dafa846aff473ca3b93e
x1Dman <glaider102@mail.ru>
2019-11-01 20:20:03 +0300

    Scripted auto-commit on change (2019-11-01 20:20:03) by gitwatch.sh

--- src/main/java/bmstu/lab4/JSAkkaTester.java
+++ src/main/java/bmstu/lab4/JSAkkaTester.java
@@ -6,6 +6,7 @@ import akka.actor.ActorSystem;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.marshallers.jackson.Jackson;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.AllDirectives;
@@ -14,7 +15,7 @@ import akka.pattern.Patterns;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 import java.util.concurrent.CompletionStage;
-import java.util.concurrent.Future;
+import scala.concurrent.Future;
 
 
 import static akka.http.javadsl.server.Directives.*;
@@ -45,17 +46,23 @@ public class JSAkkaTester extends AllDirectives{
 
     private Route jsTesterRoute(){
 
-
-        return concat(
+        return route(
                 get(
                         () -> parameter("packageId", (packageId) ->
                             {
-                                Future<Object> result = Patterns.ask(mainActor,
-                                        packageId)
+                                Future<Object> result = Patterns.ask(mainActor, packageId, 5000);
+                                return completeOKWithFuture(result, Jackson.marshaller());
                             }
                         )
-                )
-        );
+                ),
+                post(
+                        () -> {
+                            return entity(Jackson.unmarshaller(PackageDecoded.class),
+                                    msg -> {
+                                        mainActor.tell(msg, ActorRef.noSender());
+                                        return complete("");
+                                    });
+                        }));
     }
 
 }
