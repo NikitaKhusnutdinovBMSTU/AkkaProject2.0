b1e0b94fa1795a3b1670265fef6cf0ef9520deef
x1Dman <glaider102@mail.ru>
2019-11-01 19:36:04 +0300

    Scripted auto-commit on change (2019-11-01 19:36:04) by gitwatch.sh

---
+++ src/main/java/bmstu/lab4/JSAkkaTester.java
@@ -0,0 +1,39 @@
+package bmstu.lab4;
+
+import akka.NotUsed;
+import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.server.AllDirectives;
+import akka.http.javadsl.server.Route;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+import java.util.concurrent.CompletionStage;
+
+public class JSAkkaTester extends AllDirectives{
+
+    public static void main(String[] args) throws Exception{
+
+        ActorSystem system = ActorSystem.create("routes");
+
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        JSAkkaTester app = new JSAkkaTester();
+
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = app.createRoute().flow(system, materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(routeFlow, ConnectHttp.toHost("localhost", 8080), materializer);
+        System.out.println("Server online on localhost:8080/");
+        System.in.read();
+
+        binding.thenCompose(ServerBinding::unbind).thenAccept(unbound -> system.terminate());
+
+    }
+
+    private Route createRoute(){
+        return concat(path("test", () -> get(() -> complete("hello"))));
+    }
+
+}
