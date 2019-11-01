e1e7a0cb7df3b8c7c9dc3ee98876b98b25ca4a63
x1Dman <glaider102@mail.ru>
2019-11-01 20:01:39 +0300

    Scripted auto-commit on change (2019-11-01 20:01:39) by gitwatch.sh

--- src/main/java/bmstu/lab4/PackageDecoded.java
+++ src/main/java/bmstu/lab4/PackageDecoded.java
@@ -1,4 +1,30 @@
 package bmstu.lab4;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class PackageDecoded {
+    private final int packageId;
+    private final String jsScript;
+    private final String functionName;
+    private final TestsDecoded[] tests;
+
+    @JsonCreator
+    public PackageDecoded(@JsonProperty("packageId") String packageId,
+                          @JsonProperty("jsSript") String jsScript,
+                          @JsonProperty("functionName") String functionName,
+                          @JsonProperty("tests") TestsDecoded[] tests){
+        this.packageId = Integer.parseInt(packageId);
+        this.jsScript = jsScript;
+        this.functionName = functionName;
+        this.tests = tests;
+    }
+
+    public int getPackageId(){
+        return packageId;
+    }
+
+    public String getFunctionName(){
+        return functionName;
+    }
 }
--- src/main/java/bmstu/lab4/TestsDecoded.java
+++ src/main/java/bmstu/lab4/TestsDecoded.java
@@ -1,4 +1,19 @@
 package bmstu.lab4;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class TestsDecoded {
+    private final String testName;
+    private final float expectedResult;
+    private final int[] params;
+
+    @JsonCreator
+    public TestsDecoded(@JsonProperty("testName") String testname,
+                        @JsonProperty("expectedResult") String expectedResult,
+                        @JsonProperty("params") int[] params){
+        this.testName = testname;
+        this.expectedResult = Float.parseFloat(expectedResult);
+        this.params = params;
+    }
 }
