3ac6e618cb01ad646469ca5348add6006df5f046
x1Dman <glaider102@mail.ru>
2019-11-01 23:59:13 +0300

    Scripted auto-commit on change (2019-11-01 23:59:12) by gitwatch.sh

--- src/main/java/bmstu/lab4/Storage.java
+++ src/main/java/bmstu/lab4/Storage.java
@@ -17,7 +17,7 @@ public class Storage extends AbstractActor {
                 req -> getSender().tell(data.get(req.getPackageId()),
                         ActorRef.noSender()))
                 .match(PackageDecoded.class, msg -> {
-                    System.out.println("MSG->" + msg);
+                    System.out.println("MSG->" + msg.getFunctionName());
                     data.add(msg);
                 })
                 .build();
