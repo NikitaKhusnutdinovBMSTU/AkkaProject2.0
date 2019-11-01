fbc5e73571b381d4c7ea0c39711604fa44c8f645
x1Dman <glaider102@mail.ru>
2019-11-01 20:12:15 +0300

    Scripted auto-commit on change (2019-11-01 20:12:15) by gitwatch.sh

--- src/main/java/bmstu/lab4/JSAkkaTester.java
+++ src/main/java/bmstu/lab4/JSAkkaTester.java
@@ -1,6 +1,7 @@
 package bmstu.lab4;
 
 import akka.NotUsed;
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
@@ -9,15 +10,19 @@ import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.Route;
+import akka.pattern.Patterns;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 import java.util.concurrent.CompletionStage;
+import java.util.concurrent.Future;
 
 
 import static akka.http.javadsl.server.Directives.*;
 import static akka.http.javadsl.unmarshalling.StringUnmarshallers.INTEGER
 
 public class JSAkkaTester extends AllDirectives{
+
+    static ActorRef mainActor;
 
     public static void main(String[] args) throws Exception{
 
@@ -32,7 +37,9 @@ public class JSAkkaTester extends AllDirectives{
         System.out.println("Server online on localhost:8080/");
         System.in.read();
 
-        binding.thenCompose(ServerBinding::unbind).thenAccept(unbound -> system.terminate());
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate());
 
     }
 
@@ -40,14 +47,14 @@ public class JSAkkaTester extends AllDirectives{
 
 
         return concat(
-                path("", () -> 
-                        getFromResource("web/index.html")
-                ),
-                pathPrefix("get", () ->
-                        path(INTEGER, packID -> concat(
-
-                                get(() -> )
-                        )))
+                get(
+                        () -> parameter("packageId", (packageId) ->
+                            {
+                                Future<Object> result = Patterns.ask(mainActor,
+                                        )
+                            }
+                        )
+                )
         );
     }
 
