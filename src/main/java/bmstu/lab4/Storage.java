c35b1a961ef69335b857363b18894895e5124e7a
x1Dman <glaider102@mail.ru>
2019-11-01 22:50:04 +0300

    Scripted auto-commit on change (2019-11-01 22:50:04) by gitwatch.sh

--- src/main/java/bmstu/lab4/Storage.java
+++ src/main/java/bmstu/lab4/Storage.java
@@ -5,12 +5,14 @@ import akka.actor.AbstractActor;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class Storage extends AbstractActor {
-    private Map<Integer, PackageDecoded> data;
+    private Set<PackageDecoded> data;
 
     @Override
     public Receive createReceive() {
+
         return null;
     }
 }
