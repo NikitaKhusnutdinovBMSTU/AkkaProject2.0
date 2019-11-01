76e8d6eeaff4c30be23726bd5ffacbb9e411a9e8
x1Dman <glaider102@mail.ru>
2019-11-01 22:02:10 +0300

    Scripted auto-commit on change (2019-11-01 22:02:10) by gitwatch.sh

--- src/main/java/bmstu/lab4/ExecuteMSG.java
+++ src/main/java/bmstu/lab4/ExecuteMSG.java
@@ -3,6 +3,7 @@ package bmstu.lab4;
 import javafx.util.Pair;
 
 public class ExecuteMSG {
+    //TODO smth more comfortable
     private Pair<Integer, PackageDecoded> msg;
 
     public ExecuteMSG(int a, PackageDecoded packageDecoded){
