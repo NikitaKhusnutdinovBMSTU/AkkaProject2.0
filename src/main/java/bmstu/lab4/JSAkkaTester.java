d2ed299bce02e571bbd8a520f1e7f9ac80ac23c2
x1Dman <glaider102@mail.ru>
2019-11-02 00:29:24 +0300

    Scripted auto-commit on change (2019-11-02 00:29:24) by gitwatch.sh

--- src/main/java/bmstu/lab4/JSAkkaTester.java
+++ src/main/java/bmstu/lab4/JSAkkaTester.java
@@ -65,7 +65,7 @@ public class JSAkkaTester extends AllDirectives{
                         () -> entity(Jackson.unmarshaller(PackageDecoded.class),
                                 msg -> {
                                     manager.tell(msg, ActorRef.noSender());
-                                    return complete("");
+                                    return complete("it's fine brah");
                                 })));
     }
 
